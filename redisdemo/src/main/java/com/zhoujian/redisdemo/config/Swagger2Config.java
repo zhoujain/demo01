9dd97366bf081e2403bc27ef7c484f1e2170a1b1
zhoujain <zhoujian0727@foxmail.com>
2022-12-23 17:54:08 +0800

    1.redis的初步搭建

---
+++ redisdemo/src/main/java/com/zhoujian/redisdemo/config/Swagger2Config.java
@@ -0,0 +1,140 @@
+package com.zhoujian.redisdemo.config;
+
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import com.zhoujian.redisdemo.constant.CommonConstant;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spring.web.plugins.WebFluxRequestHandlerProvider;
+import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Swagger的配置文件
+ *
+ * @author zhoujian on 2022/7/27
+ */
+@Configuration
+@EnableSwagger2
+@EnableKnife4j
+public class Swagger2Config {
+
+
+
+
+    @Bean(value = "defaultApi2")
+    public Docket defaultApi2() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                //此包路径下的类，才生成接口文档
+                .apis(RequestHandlerSelectors.basePackage("com.zhoujian.redisdemo"))
+                //加了ApiOperation注解的类，才生成接口文档
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build()
+                .securitySchemes(Collections.singletonList(securityScheme()))
+                .securityContexts(securityContexts());
+        //.globalOperationParameters(setHeaderToken());
+    }
+
+    @Bean
+    SecurityScheme securityScheme() {
+        return new ApiKey(CommonConstant.AUTHORIZATION, CommonConstant.AUTHORIZATION, "header");
+    }
+
+
+    /**
+     * api文档的详细信息函数,注意这里的注解引用的是哪个
+     *
+     * @return
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                // //大标题
+                .title("Redis测试API接口文档")
+                // 版本号
+                .version("1.0")
+//				.termsOfServiceUrl("NO terms of service")
+                // 描述
+                .description("后台API接口")
+                // 作者
+                .contact(new Contact("ZJ","www.xxx.com","xx"))
+                .build();
+    }
+
+
+    private List<SecurityContext> securityContexts() {
+        return new ArrayList(
+                Collections.singleton(SecurityContext.builder()
+                        .securityReferences(defaultAuth())
+                        .forPaths(PathSelectors.any())
+                        .build())
+        );
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return new ArrayList(
+                Collections.singleton(new SecurityReference(CommonConstant.AUTHORIZATION, authorizationScopes)));
+    }
+
+    /**
+     * 解决springboot2.6 和springfox不兼容问题
+     * 不加控制台报错
+     * @return
+     */
+    @Bean
+    public static BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+        return new BeanPostProcessor() {
+
+            @Override
+            public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+                if (bean instanceof WebMvcRequestHandlerProvider || bean instanceof WebFluxRequestHandlerProvider) {
+                    customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
+                }
+                return bean;
+            }
+
+            private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(List<T> mappings) {
+                List<T> copy = mappings.stream()
+                        .filter(mapping -> mapping.getPatternParser() == null)
+                        .collect(Collectors.toList());
+                mappings.clear();
+                mappings.addAll(copy);
+            }
+
+            @SuppressWarnings("unchecked")
+            private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
+                try {
+                    Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
+                    field.setAccessible(true);
+                    return (List<RequestMappingInfoHandlerMapping>) field.get(bean);
+                } catch (IllegalArgumentException | IllegalAccessException e) {
+                    throw new IllegalStateException(e);
+                }
+            }
+        };
+    }
+}
---
+++ redisdemo/src/main/java/com/zhoujian/redisdemo/domain/User.java
@@ -0,0 +1,9 @@
+package com.zhoujian.redisdemo.domain;
+
+/**
+ * 用户
+ *
+ * @author zhoujian on 2022/12/23
+ */
+public class User {
+}
