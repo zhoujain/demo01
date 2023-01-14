bb245326c0c65fb1212273936f841e0f915045d6
zhoujian <1842612454@qq.com>
2023-01-14 09:22:28 +0800

    1.完善

--- template/src/main/java/com/zhoujian/template/domain/User.java
+++ template/src/main/java/com/zhoujian/template/domain/User.java
@@ -1,9 +1,21 @@
 package com.zhoujian.template.domain;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
 /**
  * 用户
  *
  * @author zhoujian on 2022/12/23
  */
-public class User {
+@Data
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String id;
+    private String name;
 }
