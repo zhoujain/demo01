4067acfdbbc45d787d84dd2297d2727d928b38fa
zhoujian <1842612454@qq.com>
2023-01-30 10:03:28 +0800

    1.切面demo

---
+++ aspectdemo/src/main/java/com/zhoujian/aspectdemo/AspectdemoApplication.java
@@ -0,0 +1,31 @@
+package com.zhoujian.aspectdemo;
+
+import com.zhoujian.aspectdemo.utils.MyConvertUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+@Slf4j
+public class AspectdemoApplication {
+
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(AspectdemoApplication.class, args);
+        ConfigurableEnvironment environment = applicationContext.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = environment.getProperty("server.port");
+        String path = MyConvertUtils.getString(environment.getProperty("server.servlet.context-path"));
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Application is running! Access URLs:\n\t" +
+                "Local: \t\thttp://localhost:" + port + path + "/\n\t" +
+                "External: \thttp://" + ip + ":" + port + path + "/\n\t" +
+                "Swagger文档: \thttp://" + ip + ":" + port + path + "/doc.html\n\t" +
+                "----------------------------------------------------------");
+    }
+
+}
---
+++ aspectdemo/src/main/java/com/zhoujian/aspectdemo/domain/User.java
@@ -0,0 +1,17 @@
+package com.zhoujian.aspectdemo.domain;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 用户
+ *
+ * @author zhoujian on 2022/12/23
+ */
+@Data
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String id;
+    private String name;
+}
